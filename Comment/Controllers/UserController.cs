09c257d0bbb369b9c0b7113a1bcebe5bf8dc6d98
Bibek Thapaliya <bvek.thapaliya@gmail.com>
2022-01-26 13:48:45 +0545

    user controller added

---
+++ Comment/Controllers/UserController.cs
@@ -0,0 +1,35 @@
+﻿using Comment.Repos.Users;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        public readonly IUserRepository _userRepository;
+
+        public UserController(IUserRepository userRepository)
+        {
+
+            _userRepository = userRepository;
+
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetUsers()
+        {
+            var user = await _userRepository.GetUsers();
+            return Ok(new { data = user });
+        }
+
+
+
+
+    }
+        
+}
