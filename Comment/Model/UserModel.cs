d3e2a5b03a3f8c6bb4071ee273c413253bab0205
Bibek Thapaliya <bvek.thapaliya@gmail.com>
2022-01-26 13:49:23 +0545

    user model added

---
+++ Comment/Model/UserModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment.Model
+{
+    public class UserModel
+    {
+        public Guid userGuid { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+
+    }
+}
