230f1e6857a848de1a38eda0f98db49d07e43565
Bibek Thapaliya <bvek.thapaliya@gmail.com>
2022-01-26 13:17:00 +0545

    Add files via upload
    Added popup dialog in Delete button

--- Comment/ClientApp/src/components/CommentList.tsx
+++ Comment/ClientApp/src/components/CommentList.tsx
@@ -1,12 +1,12 @@
 ﻿
 import * as React from 'react';
-import { Component,PureComponent } from 'react';
-import { Fragment } from 'react';
-import { Button } from 'react-bootstrap'
+import { Component } from 'react';
 import axios from 'axios';
-import { CommentBox } from './CommentBox';
-//import {EditCommentRow} from './EditCommentRow'
 import { Link } from 'react-router-dom';
+import Popup from 'reactjs-popup';
+import 'reactjs-popup/dist/index.css';
+import { useState, useEffect } from 'react';
+import { event } from 'jquery';
 
 
 
@@ -16,144 +16,111 @@ interface IcommentListProps {
 
 }
 
-
 interface ICommentListState {
-   // loading: boolean;
-    commentlist: any[];
-    editCommentId: string;
-    editFormData: object;
   
-
+    commentlist: any[];
+   
 }
 
+ function  CommentList(IcommentListProps,ICommentListState) {
 
-export class CommentList extends PureComponent<IcommentListProps, ICommentListState>{
-
-
-
- constructor(props:any) {
-        super(props);
-     this.state = {
-         commentlist: [],
-         editCommentId:'',
-         editFormData:{
-             firstName: '',
-             lastName: '',
-             Comment:''
-         }
-           
-         
-     };
-
-     //this.populateCommentdata = this.populateCommentdata.bind(this);
-     this.renderCommentTable = this.renderCommentTable.bind(this);
-     this.onDeleteData = this.onDeleteData.bind(this);
-     //this.onEditData = this.onEditData.bind(this);
-    // this.handleCancelClick = this.handleCancelClick.bind(this);
-     //this.onEditFormChange = this.onEditFormChange.bind(this);
+    const [commentlist, setCommentlist] = useState([]);
     
-    
+
+     useEffect(() => {
+           populateCommentdata();
+    }, []);
+
+    const onDeleteData = async (event,commentid: String) => {
+        event.preventDefault();
+        let id = {
+            CommentId: commentid,
+        };
+        console.log(id);
+        let response = await axios({
+            method: "DELETE",
+            url: "/Comment",
+            data: id,
+            headers: { "Content-type": "application/json" },
+        }).then((response) => {
+            populateCommentdata();
+
+        })
+            .catch((error) => {
+                console.log(error)
+            })
 
     }
 
-    componentDidMount() {
-        this.populateCommentdata();
-    }
-
-   
-
-
-
-   
-    
-     async  onDeleteData(commentid:String) {
-
-         let id = {
-             CommentId: commentid,
-                  };
-         console.log(id);
-         let response = await axios({
-             method: "DELETE",
-             url: "/Comment",
-             data: id,
-             headers: { "Content-type": "application/json" },
-         }).then((response) => {
-             this.populateCommentdata();
-             
-         })
-             .catch((error) => {
-                 console.log(error)
-             })
-
-    }
-
-           
-    async populateCommentdata() {
+     const populateCommentdata = async () => {
         const response = await fetch('/Comment');
         const data = await response.json();
-        this.setState({ commentlist: data.data });
+        { setCommentlist(data.data) };
     }
 
-    public  renderCommentTable(commentlist:any) {
+    const renderCommentTable = (commentlist: any) => {
 
-        var count = 1 
+        var count = 1
         return (
             <form>
-            <table className='table table-striped' aria-labelledby="tabelLabel">
-                <thead>
-                    <tr>
-                        <th>S.n</th>
-                        <th>firstName</th>
-                        <th>lastName</th>
-                        <th>Comment</th>
-                        <th>Action</th>
+                <table className='table table-striped' aria-labelledby="tabelLabel">
+                    <thead>
+                        <tr>
+                            <th>S.n</th>
+                            <th>firstName</th>
+                            <th>lastName</th>
+                            <th>Comment</th>
+                            <th>Action</th>
 
-                    </tr>
-                </thead>
-                <tbody>
-                    
-                    {commentlist.map((comment: any, index: any) =>(
-                        <><tr key={comment.commentGuid}>
+                        </tr>
+                    </thead>
+                    <tbody>
+
+                        {commentlist.map((comment: any, index: any) => (
+                            <><tr key={comment.commentGuid}>
                                 <td>{count++}</td>
                                 <td>{comment.firstName}</td>
                                 <td>{comment.lastName}</td>
                                 <td>{comment.comment}</td>
                                 <>
-                                    <div className='row'>
-                                     <button className="btn-secondary" onClick={() => this.onDeleteData(comment.commentGuid)}>Delete
-                                     </button>
-                                
-                                    <td>&nbsp;</td>
-                                     <Link
-                                        className="btn btn-outline-primary mr-2"
-                                        to={`/editComment/${comment.commentGuid}`}
-                                    >
-                                        Edit
-                                    </Link>
+                                    <div className='row md -4'>
+                                        <Popup trigger={<button>Delete</button>}
+                                            position="right center">
+                                            <div>Are You sure to delete data</div>
+                                            <button onClick={(event) => this.onDeleteData(comment.commentGuid,event)}>Yes</button>
+                                        </Popup>
+
+                                        <td>&nbsp;</td>
+                                        <Link
+                                            className="btn btn-outline-primary mr-2"
+                                            to={`/editComment/${comment.commentGuid}`}
+                                        >
+                                            Edit
+                                        </Link>
                                     </div>
-                                  </>
+                                </>
                             </tr></>
-                        
-                    ))}
-                   
-                </tbody>
+
+                        ))}
+
+                    </tbody>
                 </table>
-                </form>
-             
+            </form>
+
         );
     }
 
 
-    render() {
+    return (
+        <div>
+            <h1 id="tabelLabel" >CommentList</h1>
+            {renderCommentTable(commentlist)}
+        </div>
+    );
 
-        let contents = this.renderCommentTable(this.state.commentlist);
-
-        return (
-            <div>
-                <h1 id="tabelLabel" >CommentList</h1>
-                 {contents}
-            </div>
-        );
-    }
 
 }
+
+
+export default React.memo(CommentList);
+
