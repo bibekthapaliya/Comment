f11548131a24d0b5457822632717660c6f48a80c
Bibek Thapaliya <bvek.thapaliya@gmail.com>
2022-01-26 13:51:01 +0545

    user repo created

---
+++ Comment/Repos/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment.Repos.Users
+{
+    public class User
+    {
+        public Guid userGuid { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+    }
+}
---
+++ Comment/Repos/UserRepository.cs
@@ -0,0 +1,49 @@
+﻿using Comment.Providers;
+using Dapper;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Comment.Repos.Users
+{
+    public interface IUserRepository
+    {
+
+
+
+        Task<IList<User>> GetUsers();
+
+
+
+
+
+    };
+
+    public class UserRepository : BaseRepository, IUserRepository
+    {
+
+
+        public UserRepository(IConfiguration configuration) : base(configuration)
+        {
+
+        }
+
+        public async Task<IList<User>> GetUsers()
+        {
+            using (IDbConnection conn = Connection)
+            {
+                var sql = @"SELECT * FROM users";
+                var userlist = await conn.QueryAsync<User>(sql);
+                return userlist.ToList();
+            }
+        }
+
+        
+    }
+
+
+
+    }
